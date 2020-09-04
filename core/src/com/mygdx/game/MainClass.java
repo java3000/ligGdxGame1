9e5eea6e1a67362926dde5e15c70ae451b69982e
manunia <timoshenkomaria@mail.ru>
2020-09-04 16:17:40 +0300

    добавлен viewport для корректного изменения размеров экрана с изображением

--- core/src/com/mygdx/game/MainClass.java
+++ core/src/com/mygdx/game/MainClass.java
@@ -3,16 +3,11 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.game.state.GameStateManager;
 import com.mygdx.game.state.MenuState;
-
-import java.util.Random;
 
 public class MainClass extends ApplicationAdapter {
 	private GameStateManager gsm;
